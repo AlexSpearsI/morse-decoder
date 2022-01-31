9f9e7c4950209cb12ff00475356fe9803b3b853e
alexspearsi <strelchenko.aleksandr@yandex.ru>
2022-01-31 05:18:14 +0300

    add: finished

--- src/index.js
+++ src/index.js
@@ -38,7 +38,35 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+
+    let arrayWithWords = expr.split("**********");
+    let mainArray = [];
+    let result = '';
+    
+    for (let word of arrayWithWords) {
+      for (let i = 0; i < word.length; i += 2) {
+        let item = word.slice(i, i + 2);
+        (item == '00') ? mainArray.push(' ') :
+          (item == '10') ? mainArray.push('.') :
+            (item == '11') ? mainArray.push('-') : mainArray;
+      }
+      mainArray.push("\n");
+    }
+    
+    
+    mainArray.length = mainArray.length - 1;
+    
+    for (let i = 0; i < mainArray.length; i += 5) {
+      if (mainArray[i] == '\n') {
+        i++;
+        result = result + ' ';
+      }
+    
+      let solution = mainArray.slice(i, i + 5).join('').trim();
+      result = result + MORSE_TABLE[solution];
+    }
+    
+    return result;
 }
 
 module.exports = {
